b7d5b3dc7033d4f59b3f8054c44b71eb7cf8d811
movavi_school <Kovalenkoaa2008@icloud.com>
2023-09-08 21:20:06 +0700

    повтор прошлого года

---
+++ lesson 1/main.py
@@ -0,0 +1,54 @@
+# 1 задача
+# name = 'Роберт Першинг Уодлоу'
+# age = '22 года'
+# rost = '272 см'
+# print(name)
+# print(age)
+# print(rost)
+# 2 задача
+# name = input('твоё имя?')
+# rost = int(input('твой рост?'))
+# gugu = (272 - rost)
+# print(name,'ты ниже Роберт Першинг Уодлоу', 'на',gugu )
+# 3 задача
+
+# chveta = input('введи цвет?')
+# chveta2 = input('введи цвет?')
+# a = 'красный'
+# b = 'синий'
+# c = 'жёлтый'
+# if chveta == a and chveta2 == b or chveta == b and chveta2 == a:
+#     print('фиолетовый')
+#
+# if chveta == a and chveta2 == c or chveta == c and chveta2 == a:
+#     print('оранжевый')
+#
+# if chveta == b and chveta2 == c or chveta == c and chveta2 == b:
+#     print('зелёный')
+# 4 задача
+# chislo = int(input('число?'))
+# chislo2 = int(input('число?'))
+# for i in range (chislo,chislo2 + 1):
+#     print(i ** 2)
+# 5 задача
+# chislo = int(input('число?'))
+# chislo2 = int(input('число?'))
+# for i in range (chislo,chislo2 + 1):
+#     print(i ** 3)
+# 6 задача
+# n = int(input('введи число: '))
+#
+# factorial = 1
+#
+# for i in range(2, n + 1):
+#     factorial *= i
+#
+# print(factorial)
+# 7 задача
+organizm = int(input('стартовое количество организмов? '))
+popylachiya = input('среднее суточное увелечение популяции? ')
+den = int(input('сколько дней?'))
+for i in range(1,den+1):
+    print(i)
+
+
